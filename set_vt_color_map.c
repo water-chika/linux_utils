e60ca1c3da8e5130adea439d2135e80a2128edfa
Water Chika <water_chika@outlook.com>
2025-11-04 00:20:59 +0000

    add set_vt_color_map

---
+++ set_vt_color_map.c
@@ -0,0 +1,45 @@
+#include <fcntl.h>
+#include <linux/vt.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <limits.h>
+#include <poll.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include <linux/kd.h>
+
+int main(void) {
+    int fd = open("/dev/tty0", O_RDWR);
+    assert(fd);
+    uint8_t cmap[] = {
+        0xff, 0xff, 0xff,
+        0x00, 0x00, 0x00,
+        0xff, 0xff, 0xff,
+        0x00, 0x00, 0x00,
+        0xff, 0xff, 0xff,
+        0x00, 0x00, 0x00,
+        0xff, 0xff, 0xff,
+        0x00, 0x00, 0x00,
+        0xff, 0xff, 0xff,
+        0x00, 0x00, 0x00,
+        0xff, 0xff, 0xff,
+        0x00, 0x00, 0x00,
+        0xff, 0xff, 0xff,
+        0x00, 0x00, 0x00,
+        0xff, 0xff, 0xff,
+        0x00, 0x00, 0x00,
+    };
+    int ret = ioctl(fd, PIO_CMAP, cmap);
+    if (ret != 0) {
+        printf("GIO_CMAP ret: %d\n", ret);
+    }
+    ret = ioctl(fd, PIO_CMAP, cmap);
+    if (ret != 0) {
+        printf("PIO_CMAP ret: %d\n", ret);
+    }
+    close(fd);
+    return 0;
+}
